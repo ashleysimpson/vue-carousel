be8303242529dd8fed6ee94d9ba7bb5ad89e12d4
ashleysimpson <ashley.simpson@mail.mcgill.ca>
2019-04-02 16:30:53 -0400

    feat: remove all old files and add new file/folder structure

--- src/PaginationDot.vue
+++ src/PaginationDot.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: "carousel"
+  name: "PaginationDot.vue"
 };
 </script>
 
--- src/utils/NavigationButton.vue
+++ src/utils/NavigationButton.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: "carousel"
+  name: "NavigationButton.vue"
 };
 </script>
 
